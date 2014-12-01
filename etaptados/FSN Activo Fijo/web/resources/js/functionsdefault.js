d28eb94caf93ddbb75fa1fe56d553ecc38eac6fd
jazonder@gmail.com <jazonder@gmail.com@295aecc3-7543-b627-607c-5cb3b45b8cb5>
2014-12-01 03:12:35 +0000

    menu responsive listo, solo falta agregar las opciones y filtrar, lo hare en esta proxima actualizacion.

--- etaptados/FSN Activo Fijo/web/resources/js/functionsdefault.js
+++ etaptados/FSN Activo Fijo/web/resources/js/functionsdefault.js
@@ -1,6 +1,2 @@
-function resizeIframe(idIframe){
-                var miIframe=document.getElementById(idIframe);
-                var alturaPagina=miIframe.contentWindow.document.body.scrollHeight+50;
-                    miIframe.style.height=alturaPagina+"px"; 
-                }
+
                
